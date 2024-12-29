c3cb52522dac904e6ffb414b94d769dfe5a035d9
MasterD2012 <r002960@rainhillhigh.org.uk>
2024-12-29 08:46:36 +0000

    Delete frontend directory

--- frontend/scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('loginForm').onsubmit = (e) => {
-    e.preventDefault();
-    alert('Login functionality coming soon!');
-};
-
-document.getElementById('uploadForm').onsubmit = (e) => {
-    e.preventDefault();
-    alert('Upload functionality coming soon!');
-};
