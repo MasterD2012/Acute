fb29acb5d157ff21fa8539af06db1faa9abf1313
MasterD2012 <r002960@rainhillhigh.org.uk>
2024-12-29 08:42:53 +0000

    Add files via upload

---
+++ frontend/scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('loginForm').onsubmit = (e) => {
+    e.preventDefault();
+    alert('Login functionality coming soon!');
+};
+
+document.getElementById('uploadForm').onsubmit = (e) => {
+    e.preventDefault();
+    alert('Upload functionality coming soon!');
+};
